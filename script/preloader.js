531695603694bbebcdd845a6d447eb44140e38b9
artspektor <artspektor96@gmail.com>
2018-10-14 20:48:13 +0300

    add nav, add carousel
    add nav, add carousel
    
    Need img for carousel

---
+++ script/preloader.js
@@ -0,0 +1,9 @@
+$(window).load(function() {
+		// Preloader
+		$('.intro-tables, .parallax, header').css('opacity', '0');
+		$('.preloader').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+			$('.preloader').hide();
+			$('.parallax, header').addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				$('.intro-tables').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+			});
+		});
